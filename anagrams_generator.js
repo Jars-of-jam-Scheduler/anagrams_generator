92c4bd6a80827fa505acc99102149b1aa9638a39
Dorian VEGARA <dorian.vegara@gmail.com>
2016-10-24 14:44:15 +0200

    The algorithm now works with a string. The final-user now can calculate the anagrams and/or display them. Information messages now appear in the Web browser's console.

--- anagrams_generator.js
+++ anagrams_generator.js
@@ -1,5 +1,5 @@
 /**
- * This recursive function finds all the anagrams from its first parameter.
+ * This recursive algorithm finds all the anagrams from its parameter.
  * Its working is described below.
  *
  * The algorithm goes through a beginning array.
@@ -10,52 +10,51 @@
  *  2. Recursive call : a new algorithm's execution is launched (taking account of N°1) ;
  *  3. The algorithm sets it "unfixed" ;
  *  4. END ;
- *  The current anagram (id est : the one which is being constructed) is constructed little by little thanks to N°2
+ *  NB : The current anagram (id est : the one which is being constructed) is constructed little by little thanks to N°2
  *  (as it's passed as a function's argument).
+ *  Each time the current anagram has the same size than the beginning array, the called function returns.
  */
 
-var all_found_anagrams = [];
-
-const BEGINNING_ARRAY_SIZE = 3;
-var beginning_array = [];
-
-var array_are_fixed_chars = []; // Indicates which element is fixed (or not) ; TRUE <-> fixed ; FALSE <-> unfixed
-
 /**
- * Utility function
- */
-function fillArrays() {
-    for(var i = 0; i < BEGINNING_ARRAY_SIZE; i++) {
-        beginning_array.push(i);
-        array_are_fixed_chars.push(false);
-    }
-}
-fillArrays();
-
-function displayResult() {
-    console.log("This program has found " + all_found_anagrams.length + " anagrams from the string \"" + beginning_array + "\"");
-    console.log("NB : indeed, " + BEGINNING_ARRAY_SIZE + "! equals " + all_found_anagrams.length);
-    console.log("Found anagrams : " + all_found_anagrams);
-}
-
-/**
- * The core of this program. It's the recursive algorithm, which finds all the anagrams of an array
+ * Destined to be called by the final-user, giving the parameter used to construct its anagrams
  * @param beginning_array
- * @param array_are_fixed_chars
- * @param in_construction_anagram the current anagram (id est : the one which is being constructed)
+ * @returns {generateAnagrams}
  */
-function recursiveGenerateAnagrams(beginning_array, array_are_fixed_chars, in_construction_anagram) {
-    if(in_construction_anagram.length == beginning_array.length) { // An anagram is found, we store it
-        return all_found_anagrams.push(in_construction_anagram);
+function generateAnagrams(beginning_array) {
+    beginning_array = beginning_array.split("");
+    var all_found_anagrams = [];
+    var array_are_fixed_chars = []; // Indicates which element is fixed (or not) ; TRUE <-> fixed ; FALSE <-> unfixed
+    beginning_array.forEach(() => array_are_fixed_chars.push(false));
+
+    this.display = function() {
+        console.log("Rendering the results - it may take time.");
+        console.log("Found anagrams : " + all_found_anagrams);
+        console.log("%cThis program has found " + all_found_anagrams.length + " anagrams from the string \"" + beginning_array + "\"", "color : green; font-weight : bold;");
+        console.log("%cNB : indeed, " + beginning_array.length + "! equals " + all_found_anagrams.length, "color : green;");
+    };
+
+    /**
+     * The core of this program. It's the recursive algorithm, which finds all the anagrams of an array
+     * @param beginning_array
+     * @param array_are_fixed_chars
+     * @param in_construction_anagram the current anagram (id est : the one which is being constructed)
+     */
+    function recursiveGenerateAnagrams(beginning_array, array_are_fixed_chars, in_construction_anagram) {
+        if (in_construction_anagram.length == beginning_array.length) { // An anagram is found, we store it
+            return all_found_anagrams.push(in_construction_anagram);
+        }
+
+        beginning_array.forEach((char, index) => {
+            if (!array_are_fixed_chars[index]) {
+                array_are_fixed_chars[index] = true;
+                recursiveGenerateAnagrams(beginning_array, array_are_fixed_chars, in_construction_anagram + char);
+                array_are_fixed_chars[index] = false;
+            }
+        });
     }
 
-    beginning_array.forEach(function(char) {
-        if(!array_are_fixed_chars[beginning_array.indexOf(char)]) {
-            array_are_fixed_chars[beginning_array.indexOf(char)] = true;
-            recursiveGenerateAnagrams(beginning_array, array_are_fixed_chars, in_construction_anagram + char);
-            array_are_fixed_chars[beginning_array.indexOf(char)] = false;
-        }
-    });
+    console.log("Calculating the anagrams of \"" + beginning_array + "\" - it may take time. A confirmation message will appear when this calculation will be done.");
+    recursiveGenerateAnagrams(beginning_array, array_are_fixed_chars, ""); // The first call
+    console.log("This is the confirmation message. Calculation is done : the anagrams are calculated.");
+    return this;
 }
-recursiveGenerateAnagrams(beginning_array, array_are_fixed_chars, "");
-displayResult();
